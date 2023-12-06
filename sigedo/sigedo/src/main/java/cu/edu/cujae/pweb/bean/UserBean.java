bc21aa9ab7cddb6f0efa1a762a0a89aa8ebcda8e
RoxyAlm <144479190+RoxyAlm@users.noreply.github.com>
2023-12-05 19:49:56 -0500

    Update UserBean.java

--- sigedo/sigedo/src/main/java/cu/edu/cujae/pweb/bean/UserBean.java
+++ sigedo/sigedo/src/main/java/cu/edu/cujae/pweb/bean/UserBean.java
@@ -1,12 +1,31 @@
 package cu.edu.cujae.pweb.bean;
 
-import java.io.IOException;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import cu.edu.cujae.pweb.dto.UserAuthenticatedDto;
+import cu.edu.cujae.pweb.security.CurrentUserUtils;
+import cu.edu.cujae.pweb.security.UserPrincipal;
+import cu.edu.cujae.pweb.service.AuthService;
+import cu.edu.cujae.pweb.utils.JsfUtils;
+
+@Component
 @ManagedBean
+@ViewScoped
 public class UserBean {
 	
 	private String username;
@@ -48,5 +67,26 @@ public class UserBean {
 	
 	protected FacesContext getFacesContext() {
 	    return FacesContext.getCurrentInstance();
+	}
+		public String logout() {
+		return dispatchToUrl("/logout");
+	}
+	
+	public String getUserLogued() {
+		return CurrentUserUtils.getFullName();
+	}
+	
+	private String dispatchToUrl(String url) {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		HttpServletRequest request = (HttpServletRequest) facesContext.getExternalContext().getRequest();
+		HttpServletResponse response = (HttpServletResponse) facesContext.getExternalContext().getResponse();
+		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+		try {
+			dispatcher.forward(request, response);
+			facesContext.responseComplete();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}  
+		return null;
 	}
 }
