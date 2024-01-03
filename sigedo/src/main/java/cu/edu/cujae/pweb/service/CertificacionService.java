ecb99650e9a0747c91a86f5b181caefdce826836
RoxyAlm <144479190+RoxyAlm@users.noreply.github.com>
2024-01-03 14:33:45 -0500

    Create CertificacionService.java

---
+++ sigedo/src/main/java/cu/edu/cujae/pweb/service/CertificacionService.java
@@ -0,0 +1,10 @@
+package cu.edu.cujae.pweb.service;
+
+import java.util.List;
+
+import cu.edu.cujae.pweb.dto.CertificacionDto;
+
+public interface CertificacionService {
+    
+	List<CertificacionDto> getCertificaciones();
+}
